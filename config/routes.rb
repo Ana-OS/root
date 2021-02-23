a414c9e6d4c8a3b179582500e0674f42345eb962
ana sanots <anasoliveiraaa@gmail.com>
2021-02-23 20:41:05 +0000

    routes and gems added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :plants, only: [:new, :index, :edit, :update]
+    member do
+      delete :unlike
+      get :like
+    end
+
+    resources :reviews, only: [ :new, :create, :edit, :update ]
+  end
+
+    resources :stores, only: [:index]
+
+    resources :reviews, only: :destroy
 end
